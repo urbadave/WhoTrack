3a00d74bb0ab108aba3d7ab80ce7e1cfe07daf4e
David Urbanek <urbanek62@gmail.com>
2020-05-08 21:47:25 -0600

    Initial

---
+++ encounter.js
@@ -0,0 +1,26 @@
+module.exports = class Encounter2 {
+    constructor(name, date, other) {
+        this.name = name;
+        this.date = date;
+        this.other = other;        
+    }
+
+    get info() {
+        return this.getString();
+    }
+
+    getString() {
+        const hasDate = (this.date && this.date.toDateString);
+        const hasOther = this.other;
+
+        if(hasDate && hasOther){
+            return `${this.date.toDateString()}: ${this.name}. ${this.other}`;
+        } else if (hasDate){
+            return `${this.date.toDateString()}: ${this.name}`;
+        } else if (hasOther){
+            return `No Date: ${this.name}. ${this.other}`;
+        } else {
+            return `No Date: ${this.name}`;
+        }
+    };
+}
---
+++ tracker.js
@@ -0,0 +1,9 @@
+var Encounter = require('./encounter');
+
+let encList = [];
+encList.push(new Encounter('Brandon Mauhar', new Date(2020, 4, 27)));
+encList.push(new Encounter('Briane Evans', new Date(2020, 5, 3)));
+encList.push(new Encounter('Brandon Mauhar', new Date(2020, 5, 4)));
+encList.push(new Encounter('Erin Johnson', new Date(2020, 5, 8), 'Dog groomer'));
+
+encList.forEach(item => console.log(item.info));
